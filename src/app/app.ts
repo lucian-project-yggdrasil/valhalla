e7425899ced27599d6bf4e6969bf68b539510abe
Lucian Barrios <248139684+lucianbarrios@users.noreply.github.com>
2025-12-09 19:04:04 +0800

    chore: setup prettier and eslint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { greet } from '@lucian/runes';
+import { Component, OnInit, signal } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
+import { greet } from "@lucian/runes"
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.scss",
 })
-export class App {
-  protected readonly title = signal('valhalla');
-  protected readonly message = signal('Connecting...');
+export class App implements OnInit {
+	protected readonly title = signal("valhalla")
+	protected readonly message = signal("Connecting...")
 
-  ngOnInit() {
-    // 1. Call the library
-    const response = greet('Lucian');
-    this.message.set(response);
+	ngOnInit() {
+		// 1. Call the library
+		const response = greet("Lucian")
+		this.message.set(response)
 
-    // 2. Log to console to verify Zoneless is happy
-    console.log('Valhalla Initialized via Runes:', this.message());
-  }
+		// 2. Log to console to verify Zoneless is happy
+		console.log("Valhalla Initialized via Runes:", this.message())
+	}
 }
